de2d9f38eca515a1a15a18b31c3f3515d0697625
nealwangzi <815145309@qq.com>
2015-08-27 18:49:47 +0800

    新建项目

---
+++ WeiBo/WeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiBo
+//
+//  Created by Apple on 15/8/27.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeiBo/WeiBo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiBo
+//
+//  Created by Apple on 15/8/27.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
